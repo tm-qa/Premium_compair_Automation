c8ee65cdd1a89072c35e457d9fe9b9af338d2b0b
Sayali Dange <sayali.dange@turtlemint.com>
2025-05-21 17:26:42 +0530

    tm changes

--- src/main/java/com/qa/turtlemint/util/TestUtil.java
+++ src/main/java/com/qa/turtlemint/util/TestUtil.java
@@ -248,6 +248,34 @@ public class TestUtil {
         }
     }
 
+    public static void writePremiumDataTm(String filePath, List<String[]> dataRows) {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("PremiumData");
+
+        // Header row
+        Row header = sheet.createRow(0);
+        String[] headers = { "RegistrationNumber", "Make and Model", "Fuel","Variant" , "Previous Policy Type", "Insurer", "Premium" };
+        for (int i = 0; i < headers.length; i++) {
+            header.createCell(i).setCellValue(headers[i]);
+        }
+
+        // Data rows
+        int rowIndex = 1;
+        for (String[] rowData : dataRows) {
+            Row row = sheet.createRow(rowIndex++);
+            for (int i = 0; i < rowData.length; i++) {
+                row.createCell(i).setCellValue(rowData[i]);
+            }
+        }
+
+        try (FileOutputStream fos = new FileOutputStream(filePath)) {
+            workbook.write(fos);
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
